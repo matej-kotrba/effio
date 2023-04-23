cd922b579c5973699df58a4e3224a872c42f1f86
mkit2009 <matejkotrba.skola@gmail.com>
2023-04-23 19:49:31 +0200

    Home page progress

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,26 @@
+<script lang="ts">
+</script>
+
 <main>
-	<div class="flex w-full py-10 hero__section h-fit">
-		<div class="rounded-3xl bg-light_transparent_blue w-[40%] container mx-auto p-12">
-			<h1 class="font-bold text-h1 text-light_white">Effio</h1>
-			<p class="text-light_white w-[80%] text-h5">
-				Boost yout productivity, create tests for yourself and others and learn all in a modern user
-				friendly enviroment. Introducing <b>Effio</b> - online test creation tool which will help you
-				achieve all of that.
-			</p>
+	<div class="py-20 hero__section">
+		<div class="container flex w-full py-10 mx-auto h-fit">
+			<div class="rounded-3xl bg-light_transparent_blue w-[40%] container mx-auto p-12">
+				<h1 class="font-bold text-h1 text-light_white">Effio</h1>
+				<p class="text-light_white w-[80%] text-h5">
+					Boost yout productivity, create tests for yourself and others and learn all in a modern
+					user friendly enviroment. Introducing <b>Effio</b> - online test creation tool which will help
+					you achieve all of that.
+				</p>
+			</div>
+			<div class="translate-x-[-13%] translate-y-[10%] relative">
+				<img src="/imgs/test.svg" alt="Test" />
+				<div class="absolute pb-">
+					<p>Learn everything at one place</p>
+				</div>
+				<div class="absolute">
+					<p>Create, share, explore</p>
+				</div>
+			</div>
 		</div>
 	</div>
 	<div class="container" />
