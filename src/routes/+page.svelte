12d54a4d03430f145b1e661320ac80feb5381e8e
mkit2009 <matejkotrba.skola@gmail.com>
2023-04-23 18:21:23 +0200

    Setupped tailwind color variables

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main>
+	<div class="hero__section h-[500px] w-[500px]" />
+</main>
+
+<style lang="postcss">
+	.hero__section {
+		background: linear-gradient(103.97deg, #9ba3eb 0.9%, #c2a5ff 52.16%, #dbdffd 100%);
+	}
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				primary: {
+					light_primary: '#6433F0',
+					light_secondary: '#646FD4',
+					light_terciary: '#9BA3EB',
+					light_quaternary: '#DBDFFD',
+					light_text_black: '#343540',
+					light_text_black_80: '#343540cc',
+					light_text_black_60: '#34354099',
+					light_text_black_40: '#34354066',
+					light_text_black_20: '#34354033',
+					light_white: '#EFEFEF'
+				}
+			}
+		}
 	},
 	plugins: []
 };
