9b1cbfac4f5442285319ef95a58df90d6df4c24d
mkit2009 <matejkotrba.skola@gmail.com>
2023-05-02 16:08:42 +0200

    Login button basic structure

--- src/lib/components/buttons/LogInButton.svelte
+++ src/lib/components/buttons/LogInButton.svelte
@@ -1,8 +1,24 @@
 <script lang="ts">
+	import Icon from '@iconify/svelte';
+
 	export let title: string | undefined = undefined;
 	export let customStyles: string | undefined = undefined;
 </script>
 
-<button class="px-24 py-6 bg-light_primary text-light_white rounded-2xl text-h4 {customStyles}">
-	{title ?? 'Log In'}
+<!-- px-24 py-6 -->
+<button
+	class=" bg-transparent border-4 border-light_primary border-solid text-light_white rounded-full text-h4 w-64 {customStyles}"
+>
+	<div class="grid items-center gap-1 p-1">
+		{title ?? 'Log In'}
+		<div class="grid w-20 rounded-full bg-light_primary aspect-square place-items-center">
+			<Icon icon="mdi:account-unlocked" class="text-5xl text-light_white" />
+		</div>
+	</div>
 </button>
+
+<style>
+	.grid {
+		grid-template-columns: 1fr auto;
+	}
+</style>
