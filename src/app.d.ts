a2d9bdc91a33c5c5edf3838cbdc47abfb1e313b8
matej-kotrba <matekotrba.skola@gmail.com>
2023-05-05 17:56:31 +0200

    Setuping typesafety

--- src/app.d.ts
+++ src/app.d.ts
@@ -7,6 +7,13 @@ declare global {
 		// interface PageData {}
 		// interface Platform {}
 	}
+
+	declare namespace svelte.JSX {
+		interface HTMLProps<T> {
+			"on:intersect"?: (event: CustomEvent<IntersectionObserverEntry>) => void;
+			"on:unintersect"?: (event: CustomEvent<IntersectionObserverEntry>) => void;
+		}
+	}
 }
 
 export { };
