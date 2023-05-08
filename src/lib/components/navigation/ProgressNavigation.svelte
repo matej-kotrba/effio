90749bf1e0ea860a99973d72a8c48e477f551def
mkit2009 <matejkotrba.skola@gmail.com>
2023-05-08 19:13:50 +0200

    Navigation visual

---
+++ src/lib/components/navigation/ProgressNavigation.svelte
@@ -0,0 +1,62 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import type { HTMLButtonAttributes } from 'svelte/elements';
+
+	type Part = {
+		title: string;
+		onClick: Function;
+		buttonProps?: HTMLButtonAttributes;
+	};
+
+	export let parts: Part[];
+	export let color: string = 'var(--light-primary)';
+
+	let activeCount: number = 1;
+	let underlineDiv: HTMLDivElement;
+	let sectionElement: HTMLElement;
+
+	// Sets the active button and position of underline div and calls setPosition
+	function setToActive(
+		event: MouseEvent & {
+			currentTarget: EventTarget & HTMLButtonElement;
+		},
+		index: number
+	) {
+		activeCount = index;
+		setPosition(event.currentTarget.getBoundingClientRect());
+	}
+
+	// Sets position of underline div to the position of the button
+	function setPosition(itemRect: DOMRect) {
+		underlineDiv.style.left = `${itemRect.left - sectionElement.offsetLeft}px`;
+		underlineDiv.style.width = `${itemRect.width}px`;
+	}
+
+	onMount(() => {
+		setPosition(sectionElement.children[0].getBoundingClientRect());
+	});
+</script>
+
+<section class="relative flex gap-4" bind:this={sectionElement}>
+	{#each parts as part, index}
+		<button
+			{...part.buttonProps}
+			class:disabled={part?.buttonProps?.disabled}
+			on:click={(event) => {
+				setToActive(event, index + 1);
+				part.onClick();
+			}}>{part.title}</button
+		>
+	{/each}
+	<div
+		bind:this={underlineDiv}
+		class="absolute w-0 h-1 duration-200 origin-center top-[100%] rounded-full"
+		style="background-color: {color};"
+	/>
+</section>
+
+<style>
+	.disabled {
+		color: var(--light-text-black-40);
+	}
+</style>
--- src/routes/dashboard/test-creator/+page.svelte
+++ src/routes/dashboard/test-creator/+page.svelte
@@ -2,10 +2,12 @@
 	import Space from '~components/separators/Space.svelte';
 	import Separator from '~components/separators/Separator.svelte';
 	import TemplateCard from '~components/containers/TemplateCard.svelte';
+	import ProgressNavigation from '~components/navigation/ProgressNavigation.svelte';
 
-	let templateDone: boolean = false;
+	let templateDone: boolean = true;
 	let constructingDone: boolean = false;
 	let detailsDone: boolean = false;
+	let count: number = 0;
 </script>
 
 <h2 class="text-h3 font-extralight text-light_text_black">Create your new test</h2>
@@ -13,27 +15,31 @@
 	Choose a template and make a new test using many prebuilt inputs.
 </p>
 <Space />
-<div class="text-sm breadcrumbs">
+<!-- <div class="text-sm breadcrumbs">
 	<ul>
-		<li class="text-light_text_black_60 text-body2" class:done={templateDone}>
+		<li class="text-light_text_black_80 text-body2" class:done={templateDone}>
 			Picking a template
 		</li>
-		<li class="text-light_text_black_60 text-body2" class:done={constructingDone}>
+		<li class="text-light_text_black_80 text-body2" class:done={constructingDone}>
 			Constructing a test
 		</li>
-		<li class="text-light_text_black_60 text-body2" class:done={detailsDone}>Details</li>
+		<li class="text-light_text_black_80 text-body2" class:done={detailsDone}>Details</li>
 	</ul>
-</div>
+</div> -->
+<ProgressNavigation
+	parts={[
+		{ title: 'Picking a template', onClick: () => {}, buttonProps: { disabled: true } },
+		{ title: 'Constructing a test', onClick: () => {} },
+		{ title: 'Details', onClick: () => {} }
+	]}
+	color={'var(--light-primary)'}
+/>
 <h3 class="text-h4 text-light_text_black">Start with picking a template</h3>
 <Separator w={'100%'} h={'1px'} color={'var(--light-text-black-20)'} />
 <Space />
 <TemplateCard title="Blank test" imageSrc="/imgs/svgs/empty.svg" />
 
 <style>
-	li::before {
-		color: var(--light-primary);
-		font-weight: 800;
-	}
 	.done {
 		color: var(--light-primary);
 	}
