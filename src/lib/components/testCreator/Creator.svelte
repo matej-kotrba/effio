410b6ea64bc4eeb966ffeab67a4327e556fd5e82
matej-kotrba <matekotrba.skola@gmail.com>
2023-05-15 14:24:00 +0200

    Styled a little bit more the dropdown of inputs

--- src/lib/components/testCreator/Creator.svelte
+++ src/lib/components/testCreator/Creator.svelte
@@ -11,18 +11,16 @@
 	<div class="relative flex justify-end">
 		<div class="">
 			<button
-				class="p-2 rounded-lg bg-light_secondary text-whiter"
+				class="p-2 duration-200 rounded-lg bg-light_secondary text-whiter hover:bg-primary"
 				on:click={() => (openDropdown = !openDropdown)}
 			>
-				<Icon
-					icon="ic:round-plus"
-					class="text-5xl rounded-lg text-light_white bg-light_secondary"
-				/>
+				<Icon icon="ic:round-plus" class="text-5xl rounded-lg text-light_white" />
 			</button>
 			<div
 				use:clickOutside
 				on:clickoutside={() => (openDropdown = false)}
-				class="absolute right-0 w-full grid_layout gap-4 p-4 rounded-md shadow-lg bottom-full bg-light_whiter duration-200
+				class="absolute right-0 w-full grid_layout gap-4 p-4 absoluteContainer
+				rounded-md shadow-lg bottom-[calc(100%+10px)] bg-light_whiter duration-200
 				{openDropdown ? '' : 'opacity-0 pointer-events-none'}"
 			>
 				{#each inputs as input}
@@ -41,6 +39,29 @@
 	.grid_layout {
 		display: grid;
 		grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
-		overflow: hidden;
+		overflow-y: scroll;
+		max-height: 250px;
+	}
+
+	/* width */
+	.absoluteContainer::-webkit-scrollbar {
+		width: 10px;
+	}
+
+	/* Track */
+	.absoluteContainer::-webkit-scrollbar-track {
+		background: var(--light-text-black-20);
+		border-radius: 50px;
+	}
+
+	/* Handle */
+	.absoluteContainer::-webkit-scrollbar-thumb {
+		background: var(--light-terciary);
+		border-radius: 50px;
+	}
+
+	/* Handle on hover */
+	.absoluteContainer::-webkit-scrollbar-thumb:hover {
+		background: var(--light-secondary);
 	}
 </style>
