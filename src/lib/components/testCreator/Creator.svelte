1908aec8e6b3077fe7868a1e42ae2124caacb265
matej-kotrba <matekotrba.skola@gmail.com>
2023-05-15 15:31:16 +0200

    Changed plus button and created input component

--- src/lib/components/testCreator/Creator.svelte
+++ src/lib/components/testCreator/Creator.svelte
@@ -1,25 +1,39 @@
 <script lang="ts">
 	import Icon from '@iconify/svelte';
 	import { clickOutside } from '~use/clickOutside';
+	import Input from '~components/testCreator/Input.svelte';
 
 	const inputs = ['Pick one', 'True / False', 'Multiple choice', 'Short answer', 'Long answer'];
+
+	const placedInputs: { title: string; templateName: string }[] = [
+		{
+			templateName: 'Pick one',
+			title: 'Pick one from these'
+		},
+		{
+			templateName: 'True / False',
+			title: 'Is this true or false?'
+		}
+	];
 
 	let openDropdown = true;
 </script>
 
 <div class="p-2 bg-light_white roudned-md text-light_text_black">
-	<div class="relative flex justify-end">
-		<div class="">
+	<div class="relative flex flex-col items-center justify-center gap-2">
+		<div class="flex flex-row items-center w-full gap-4 px-4">
+			<div class="w-full rounded-full h-0.5 bg-light_text_black_40" />
 			<button
-				class="p-2 duration-200 rounded-lg bg-light_secondary text-whiter hover:bg-primary"
+				class="relative z-10 p-2 duration-200 rounded-lg bg-light_secondary text-whiter hover:bg-primary"
 				on:click={() => (openDropdown = !openDropdown)}
 			>
 				<Icon icon="ic:round-plus" class="text-5xl rounded-lg text-light_white" />
 			</button>
+			<div class="w-full rounded-full h-0.5 bg-light_text_black_40" />
 			<div
 				use:clickOutside
 				on:clickoutside={() => (openDropdown = false)}
-				class="absolute right-0 w-full grid_layout gap-4 p-4 absoluteContainer
+				class="absolute right-0 w-full grid_layout gap-4 p-4 absoluteContainer z-30
 				rounded-md shadow-lg bottom-[calc(100%+10px)] bg-light_whiter duration-200
 				{openDropdown ? '' : 'opacity-0 pointer-events-none'}"
 			>
@@ -32,6 +46,9 @@
 				{/each}
 			</div>
 		</div>
+		{#each placedInputs as input}
+			<Input title={input.title} inputType={input.templateName} />
+		{/each}
 	</div>
 </div>
 
---
+++ src/lib/components/testCreator/Input.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	export let inputType: string;
+	export let title: string;
+</script>
+
+<div class="w-full p-4 rounded-lg bg-light_whiter">
+	{inputType}
+	{title}
+	<p>Tady pak bude nějaký content</p>
+</div>
