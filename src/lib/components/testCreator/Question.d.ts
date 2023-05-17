783dd0dd96b6f34b2a77f76e4f1f0310d3d42d93
matej-kotrba <matekotrba.skola@gmail.com>
2023-05-17 10:14:44 +0200

    Changed the checking of the question templates and added comment + changed the question types

--- src/lib/components/testCreator/Question.d.ts
+++ src/lib/components/testCreator/Question.d.ts
@@ -1,3 +1,5 @@
+// Content of the question, inputType determines the type of the question and questions is an array of questions and its structure
+// Apart from these two properties you can add any other properties you want for functionality of the question
 type QuestionContent =
   | {
     inputType: 'true/false';
@@ -14,7 +16,9 @@ type QuestionContent =
     correctAnswerIndex: number;
   };
 
+// Type with name and its properties retrieved from DB
+// IMPORTANT: cant promise typesafety becuase it is created in MySQL DB inside of JSON field
 type QuestionType = {
   name: string;
-  properties: QuestionContent;
+  properties: JSON;
 }
--- src/lib/trpc/router.ts
+++ src/lib/trpc/router.ts
@@ -11,6 +11,22 @@ export const router = t.router({
   }),
   getQuestionsTypes: t.procedure.query(async ({ ctx }) => {
     const questionsTypes = await ctx.prisma.questionType.findMany()
+    // Manual check if the field is correct will be performed here because of the JSON field
+    const result = []
+    for (const questionType of questionsTypes) {
+      const schema = z.object({
+        name: z.string(),
+        properties: z.object({
+          inputType: z.string(),
+          question: z.array(z.string()),
+        }).and(z.record(z.unknown()))
+      })
+      const validationResult = schema.passthrough().safeParse(questionType)
+      if (validationResult.success) {
+        result.push(validationResult.data)
+      }
+    }
+    console.log(result)
     return questionsTypes
   })
 })
