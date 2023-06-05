dd10ef23032be1a3604fc808fc2cb34a73b886ec
matej-kotrba <matejkotrba.skola@gmail.com>
2023-06-06 00:08:08 +0200

    Changed Lucia for AuthJS

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,18 +3,30 @@ import { createContext } from "./lib/trpc/context";
 import { router } from "./lib/trpc/router"
 import type { Handle } from "@sveltejs/kit";
 import { createTRPCHandle } from "trpc-sveltekit"
-import { auth } from "$lib/server/lucia";
+import { SvelteKitAuth } from "@auth/sveltekit"
+import prisma from "$lib/prisma";
+import GitHub from "@auth/core/providers/github"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { GITHUB_ID, GITHUB_SECRET, AUTH_SECRET } from "$env/static/private"
 
 const handleTRPCContext: Handle = createTRPCHandle({
   router: router,
   createContext: createContext,
 })
 
-const initializeLuciaAuth: Handle = async ({ event, resolve }) => {
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  event.locals.auth = auth.handleRequest(event);
-  return await resolve(event);
-};
+const handleAuth: Handle = SvelteKitAuth({
+  // Created PrismaAdapter with the Prisma client instance
+  adapter: PrismaAdapter(prisma),
+  providers: [GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET })],
+  secret: AUTH_SECRET,
+  callbacks: {
+    async signIn({ account, user }) {
+      return true // Default behavior for other providers
+    },
+  },
+  pages: {
+    // signIn: "/logins"
+  }
+})
 
-export const handle = sequence(handleTRPCContext, initializeLuciaAuth)
+export const handle = sequence(handleTRPCContext)
