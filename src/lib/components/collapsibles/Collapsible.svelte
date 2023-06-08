8cd811e2202b5c994f9ed54e29825603a1653adc
matej-kotrba <matejkotrba.skola@gmail.com>
2023-06-08 12:11:39 +0200

    Login page update

---
+++ src/lib/components/collapsibles/Collapsible.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+	import Icon from '@iconify/svelte';
+	import Space from '~components/separators/Space.svelte';
+
+	export let title: string;
+	export let position: 'left' | 'center' | 'right' = 'left';
+
+	const positionsClasses = {
+		left: 'mr-auto',
+		center: 'mx-auto',
+		right: 'ml-auto'
+	};
+
+	let contentRef: HTMLDivElement;
+
+	let isOpen = false;
+
+	function toggleCollapsible() {
+		isOpen = !isOpen;
+	}
+</script>
+
+<div class="parent w-fit {positionsClasses[position]} rounded-md bg-base-200">
+	<button
+		on:click={toggleCollapsible}
+		type={'button'}
+		class="{positionsClasses[
+			position
+		]} rounded-md px-4 py-2 w-full flex items-center gap-2 bg-base-300"
+		><Icon
+			icon="bxs:left-arrow"
+			rotate={2}
+			class="{isOpen ? 'rotate-90' : 'rotate-0'} duration-300"
+		/>
+		<span class="text-body1">{title}</span></button
+	>
+	<div class="content" class:active={isOpen} bind:this={contentRef}>
+		<div class="px-4">
+			<Space gap={10} />
+			<slot />
+			<Space gap={10} />
+		</div>
+	</div>
+</div>
+
+<style>
+	.content {
+		display: grid;
+		grid-template-rows: 0fr;
+		transition: 0.3s grid-template-rows ease;
+	}
+
+	.content > div {
+		overflow: hidden;
+	}
+
+	.content.active {
+		grid-template-rows: 1fr;
+	}
+</style>
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -2,25 +2,46 @@
 	import Icon from '@iconify/svelte';
 	import Separator from '~components/separators/Separator.svelte';
 	import Space from '~components/separators/Space.svelte';
+	import Collapsible from '~components/collapsibles/Collapsible.svelte';
 </script>
 
-<div class="bg">
+<div class="py-4 bg">
 	<div class="px-6 py-3 bg-white w-[100vw] max-w-[450px] rounded-md">
 		<h2 class="text-center uppercase text-h4">Login</h2>
 		<Separator w={'100%'} h={'2px'} color="var(--light-text-black-20)" />
 		<Space gap={10} />
 		<p class="text-center text-light_text_black_60">
-			For Effio we use login via providers like Google and GitHub
+			For Effio we use login via providers like <br />
+			<span class="font-bold text-light_text_black_80">Google</span>
+			and
+			<span class="font-bold text-light_text_black_80">GitHub</span>.
 		</p>
 		<Space gap={10} />
+		<div>
+			<img src="./imgs/svgs/login.svg" alt="Login illustration" />
+		</div>
+		<Space gap={10} />
+		<Collapsible title={'Why not password login?'} position={'center'}>
+			<p class="text-body2">
+				Providers like Google, GitHub, or any other can provide an easy authentication solution
+				without the need for custom usernames or passwords. This is very convenient for users
+				because they do not need to remember any login credentials, and it is also beneficial for us
+				as we do not have to store sensitive data like passwords or usernames. Additionally, this
+				approach offers enhanced security as it leverages the infrastructure and expertise of larger
+				service providers who are dedicated to safeguarding user credentials.
+			</p>
+		</Collapsible>
+		<Space gap={20} />
 		<div class="flex items-center justify-center gap-2">
 			<button
-				class="flex items-center gap-3 px-6 py-2 text-orange-400 border-2 border-orange-400 border-solid rounded-md"
+				type="button"
+				class="flex items-center gap-3 px-6 py-2 text-orange-400 duration-100 border-2 border-orange-400 border-solid rounded-md active:scale-90 hover:scale-105"
 				><Icon icon="devicon:google" /><span class="font-semibold">Google</span></button
 			>
 			<span class="text-light_text_black_40">or</span>
 			<button
-				class="flex items-center gap-3 px-6 py-2 text-black border-2 border-black border-solid rounded-md"
+				type="button"
+				class="flex items-center gap-3 px-6 py-2 text-black duration-100 border-2 border-black border-solid rounded-md active:scale-90 hover:scale-105"
 				><Icon icon="mdi:github" /><span class="font-semibold">GitHub</span></button
 			>
 		</div>
@@ -29,8 +50,7 @@
 
 <style>
 	.bg {
-		height: clamp(100vh, 100vh, 100vh);
-		width: clamp(100vw, 100vw, 100vw);
+		min-height: 100vh;
 		background-image: linear-gradient(256.86deg, #c2a5ff 12.78%, #6c63ff 90.64%);
 		display: grid;
 		place-content: center;
