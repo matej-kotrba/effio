723ce320b920d937ce8ea45874566791823f00b6
matej-kotrba <matejkotrba.skola@gmail.com>
2023-06-27 16:47:48 +0200

    Updating type system for user test data

--- src/routes/tests/[id]/+page.server.ts
+++ src/routes/tests/[id]/+page.server.ts
@@ -1,5 +1,32 @@
 import type { ServerLoad } from "@sveltejs/kit";
 
+type QuestionTransformation = {
+  [Key in keyof QuestionTypeMap]: (question: QuestionTypeMap[Key]) => object
+}
+
+// Transform the question into data which will not contain answers
+const questionTransformation: QuestionTransformation = {
+  "pickOne": (question: PickOneQuestion) => {
+    return {
+      ...question,
+      correctAnswerIndex: undefined
+    } as PartialPick<PickOneQuestion, "correctAnswerIndex">
+  },
+  "true/false": (question: TrueFalseQuestion) => {
+    return {
+      ...question,
+      answers: question.answers.map((answer) => {
+        return {
+          answer: answer,
+          isTrue: undefined
+        }
+      })
+    } as unknown as {
+        [Key in keyof TrueFalseQuestion]: Key extends "answers" ? PartialPick<TrueFalseQuestion["answers"][number], "isTrue">[] : TrueFalseQuestion[Key]
+      }
+  }
+}
+
 export const load: ServerLoad = async (request) => {
   return {
     ahoj: "world"
