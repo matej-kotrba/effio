7fba75e2343b3466f808095671a3ff9d718b70be
matej-kotrba <matejkotrba.skola@gmail.com>
2023-08-03 14:54:20 +0200

    Test created monthly graph

--- src/routes/dashboard/+page.server.ts
+++ src/routes/dashboard/+page.server.ts
@@ -19,10 +19,35 @@ export const load = async ({ locals }) => {
     LEFT(createdAt, 7) as "period" 
     FROM Test 
     WHERE ownerId = ${id}
-    GROUP BY LEFT(createdAt, 7)   
+    GROUP BY LEFT(createdAt, 7)
+    ORDER BY LEFT(createdAt, 7) ASC   
     `;
 
     summaryData = result
+
+    for (const i in summaryData) {
+      if (summaryData[+i + 1]) {
+        const currentYear = +summaryData[+i].period.slice(0, 4)
+        const nextYear = +summaryData[+i + 1].period.slice(0, 4)
+        const currentMonth = +summaryData[+i].period.slice(5, 7)
+        const nextMonth = +summaryData[+i + 1].period.slice(5, 7)
+
+        let iterator = 0
+        for (let k = 0; k < (nextYear - currentYear) * 12 + nextMonth - currentMonth; k++) {
+          summaryData.splice(+i + 1 + iterator, 0, {
+            count: 0n,
+            period: `${currentYear + Math.floor((currentMonth + k) / 12)}-${String((currentMonth + k) % 12 + 1).padStart(2, '0')}`
+          })
+          iterator++
+          // for (let k = 1; k < +summaryData[+i + 1].period.slice(5, 7) - +summaryData[i].period.slice(5, 7); k++) {
+          //   summaryData.splice(+i + 1, 0, {
+          //     count: 0n,
+          //     period: +summaryData[+i].period.slice(0, 4) + j + "-" + String((+summaryData[i].period.slice(5, 7) + k))
+          //   })
+          // }
+        }
+      }
+    }
   }
 
   return {
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -3,6 +3,12 @@
 	import type { QuestionTemplate } from '~/lib/trpc/router';
 	import { page } from '$app/stores';
 	import { onMount } from 'svelte';
+	import Chart from 'chart.js/auto';
+	import type { ChartData, ChartConfiguration } from 'chart.js/auto/auto';
+
+	export let data;
+
+	let portfolio: HTMLCanvasElement;
 
 	let templates: QuestionTemplate[] = [];
 
@@ -13,10 +19,69 @@
 	}
 
 	onMount(async () => {
+		const graphData: ChartData = {
+			labels: data.testCreationData?.map((data) => data.period),
+			datasets: [
+				{
+					label: 'Tests created',
+					data:
+						data.testCreationData?.map((data) => Number(data.count) || 0) || [],
+					backgroundColor: [
+						getComputedStyle(document.documentElement).getPropertyValue(
+							'--light-primary'
+						) || '#6722e6'
+					],
+					borderWidth: 2
+				}
+			]
+		};
+		const config: ChartConfiguration = {
+			type: 'bar',
+			data: graphData,
+			options: {
+				clip: 1,
+				borderRadius: '10',
+				responsive: true,
+				spacing: 0,
+				scales: {
+					y: {
+						beginAtZero: true,
+						ticks: {
+							precision: 0
+						}
+					}
+				},
+				plugins: {
+					legend: {
+						position: 'bottom',
+						display: true,
+						labels: {
+							usePointStyle: true,
+							padding: 10,
+							font: {
+								size: 14
+							}
+						}
+					},
+					title: {
+						display: true,
+						text: 'Tests created monthly'
+					}
+				}
+			}
+		};
+		const ctx = portfolio.getContext('2d');
+		if (ctx) {
+			let chart = new Chart(ctx, config);
+		}
+
 		getTemplates();
 	});
 </script>
 
+<div class="w-[400px]">
+	<canvas class="w-2" bind:this={portfolio} width="100" height="100" />
+</div>
 <div class="text-primary">
 	{#each templates as template}
 		<p>{template.name}</p>
