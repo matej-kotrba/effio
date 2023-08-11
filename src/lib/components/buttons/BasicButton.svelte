6c55170b1e11e2874efa5726aab7e5aba12b8a99
matej-kotrba <matejkotrba.skola@gmail.com>
2023-08-11 16:14:38 +0200

    Working on dark mode integration

--- src/lib/components/buttons/BasicButton.svelte
+++ src/lib/components/buttons/BasicButton.svelte
@@ -15,7 +15,11 @@
 	{...buttonAttributes}
 	on:click={(event) => onClick(event)}
 	class={twMerge(
-		"isolate shadow-lg shadow-light_text_black_20 relative border-none flex items-center duration-150 hover:bg-light_primary hover:text-white text-light_primary gap-2 btn bg-light_primary before:content-[''] hover:before:scale-y-0 before:origin-top before:duration-150 before:absolute before:inset-1 before:bg-white before:rounded-md before:z-[-99]",
+		`isolate shadow-lg shadow-light_text_black_20 relative border-none
+		 flex items-center duration-150 hover:bg-light_primary dark:hover:bg-dark_primary hover:text-white text-light_primary dark:text-dark_primary
+		  gap-2 btn bg-light_primary dark:bg-dark_primary before:content-[''] hover:before:scale-y-0
+			 before:origin-top before:duration-150 before:absolute before:inset-1 before:bg-white
+			  before:rounded-md before:z-[-99]`,
 		classes
 	)}
 >
--- src/lib/components/layouts/GridLayout.svelte
+++ src/lib/components/layouts/GridLayout.svelte
@@ -2,27 +2,46 @@
 	import HoverLeaning from '../effects/HoverLeaning.svelte';
 
 	export let color: string = 'bg-light_secondary';
+	export let darkColor: string = 'bg-dark_secondary';
 </script>
 
 <section>
 	<div class="a">
 		<HoverLeaning>
-			<div class="card">
-				<slot name="a" class={color?.startsWith('bg-') ? color : `bg-[${color}]`} />
+			<div
+				class={`card ${color?.startsWith('bg-') ? color : `bg-[${color}]`} ${
+					darkColor?.startsWith('bg-')
+						? 'dark:' + darkColor
+						: `dark:bg-[${darkColor}]`
+				}`}
+			>
+				<slot name="a" />
 			</div>
 		</HoverLeaning>
 	</div>
 	<div class="b">
 		<HoverLeaning>
-			<div class="card">
-				<slot name="b" class={color?.startsWith('bg-') ? color : `bg-[${color}]`} />
+			<div
+				class={`card ${color?.startsWith('bg-') ? color : `bg-[${color}]`} ${
+					darkColor?.startsWith('bg-')
+						? 'dark:' + darkColor
+						: `dark:bg-[${darkColor}]`
+				}`}
+			>
+				<slot name="b" />
 			</div>
 		</HoverLeaning>
 	</div>
 	<div class="c">
 		<HoverLeaning>
-			<div class="card">
-				<slot name="c" class={color?.startsWith('bg-') ? color : `bg-[${color}]`} />
+			<div
+				class={`card ${color?.startsWith('bg-') ? color : `bg-[${color}]`} ${
+					darkColor?.startsWith('bg-')
+						? 'dark:' + darkColor
+						: `dark:bg-[${darkColor}]`
+				}`}
+			>
+				<slot name="c" />
 			</div>
 		</HoverLeaning>
 	</div>
@@ -40,7 +59,6 @@
 		height: 100%;
 		border-radius: 40px;
 		padding: 40px;
-		@apply bg-light_secondary;
 		box-shadow: 0 0 10px rgba(0, 0, 0, 0.299);
 	}
 	.a {
