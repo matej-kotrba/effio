7fa0e0c5d41086da585fde3d74ed886767abcfc9
matej-kotrba <matejkotrba.skola@gmail.com>
2023-08-19 22:10:18 +0200

    Responsivity of homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -25,9 +25,11 @@
 				: 'hero__section'
 		}`}
 	>
-		<div class="container flex w-full py-10 mx-auto h-fit">
+		<div
+			class="container flex flex-col w-full gap-10 py-10 mx-auto lg:flex-row h-fit lg:gap-0"
+		>
 			<div
-				class="rounded-3xl bg-light_transparent_blue dark:bg-dark_quaternary w-[60%] container mx-auto p-8"
+				class="rounded-3xl bg-light_transparent_blue dark:bg-dark_quaternary lg:w-[60%] container mx-auto p-8"
 			>
 				<h1 class="font-bold text-h3 md:text-h2 xl:text-h1 text-light_white">
 					Effio
@@ -57,18 +59,35 @@
 					>
 				</div>
 			</div>
-			<div class="translate-x-[-13%] translate-y-[15%] relative">
-				<img src="/imgs/test.svg" alt="Test" class="relative z-10 w-[1000px]" />
+			<div class="lg:translate-x-[-13%] lg:translate-y-[15%] relative">
+				<img
+					src="/imgs/test.svg"
+					alt="Test"
+					class="relative z-10 mx-auto w-[100%] sm:w-[80%] lg:w-[1000px]"
+				/>
 				<div
-					class="absolute p-8 bg-light_transparent_blue dark:bg-dark_quaternary text-body1 md:text-h6 xl:text-h5 text-light_white bottom-[100%] right-0 rounded-4xl pb-40 translate-y-[60%]"
+					class="hidden lg:block absolute p-8 bg-light_transparent_blue dark:bg-dark_quaternary text-body1 md:text-h6 xl:text-h5 text-light_white bottom-[100%] right-0 rounded-4xl pb-40 lg:translate-y-[60%]"
 				>
 					<p>Learn everything at one place</p>
 				</div>
 				<div
-					class="absolute p-8 bg-light_transparent_blue dark:bg-dark_quaternary text-body1 md:text-h6 xl:text-h5 text-light_white
+					class="hidden lg:block absolute p-8 bg-light_transparent_blue dark:bg-dark_quaternary text-body1 md:text-h6 xl:text-h5 text-light_white
 				bottom-[100%] right-0 rounded-4xl pt-20 pr-40 lg:translate-y-[220%] xl:translate-y-[240%] xl:translate-x-[-10%] 2xl:translate-y-[260%] 2xl:translate-x-[-20%]"
 				>
 					<p>Create, share, explore</p>
+				</div>
+				<Space />
+				<div class="flex gap-4 lg:hidden">
+					<div
+						class="w-full p-4 text-center bg-white rounded-md dark:bg-dark_secondary"
+					>
+						<p>Learn everything at one place</p>
+					</div>
+					<div
+						class="w-full p-4 text-center bg-white rounded-md dark:bg-dark_secondary"
+					>
+						<p>Create, share, explore</p>
+					</div>
 				</div>
 			</div>
 		</div>
