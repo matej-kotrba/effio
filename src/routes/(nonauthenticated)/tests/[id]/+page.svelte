3ef74c7d7ce11bc207310d3f9d8453f7ec579990
mkit2009 <matejkotrba.skola@gmail.com>
2023-09-05 16:40:28 +0200

    Updates to test overview

--- src/routes/(nonauthenticated)/tests/[id]/+page.svelte
+++ src/routes/(nonauthenticated)/tests/[id]/+page.svelte
@@ -13,15 +13,13 @@
 	import type { TestVersion } from '@prisma/client';
 	import Collapsible from '~components/collapsibles/Collapsible.svelte';
 	import MarkSystem from '~components/testCreator/creatorUtils/MarkSystem.svelte';
+	import { MARK_LIMIT_MAX, MARK_MAX } from '~schemas/textInput.js';
+	import Separator from '~components/separators/Separator.svelte';
+	import { applicationStates } from '~stores/applicationStates';
 
 	export let data;
 
 	let isTakingTest = false;
-
-	let isSubmitting = false;
-	let submitError: string = '';
-
-	let result: QuestionServerCheckResponse<QuestionContent>[] | null = null;
 
 	let openDialog: () => void;
 
@@ -39,135 +37,98 @@
 		markSystem = null;
 	}
 
-	// $: console.log($testObject);
+	function startTest() {
+		isTakingTest = true;
+	}
 </script>
 
-{#if !isTakingTest}
-	<div
-		class="max-w-[1200px] mx-auto dark:bg-dark_light_grey p-3 rounded-md shadow-md"
-	>
-		<p class="mb-3">Test name:<br />{data.testContent.title}</p>
-		<p class="mb-3">Test description:<br />{data.testContent.description}</p>
-		<p class="flex items-center gap-1">
-			Is the test marked?
-			{#if markSystem}
-				<iconify-icon icon="ic:round-check" class="text-3xl text-green-500" />
-				<!-- <Collapsible position="center" title="Show marking">
-					{#each markSystem as mark, index}
-						{#if mark["name"] && mark["limit"]}
-							{#if markSystem[index + 1] !== undefined}
-								<p>{mark["name"]} -> {mark["limit"]}% - {markSystem[index + 1]["limit"]}%</p>
-							{/if}
-							{}
-						{/if}
-					{/each}
-				</Collapsible> -->
-			{:else}
-				<iconify-icon icon="ic:round-close" class="text-3xl text-red-600" />
-			{/if}
+<div
+	class="max-w-[1200px] mx-auto dark:bg-dark_light_grey p-3 rounded-md shadow-md"
+>
+	<h3 class="text-h3 font-extralight">Test overview</h3>
+	<div class="p-2">
+		<p class="mb-3">
+			<span
+				class="font-semibold text-light_text_black_60 dark:text-dark_text_white_60"
+				>Test name:</span
+			><br />{data.testContent.title}
+		</p>
+		<p class="mb-3">
+			<span
+				class="font-semibold text-light_text_black_60 dark:text-dark_text_white_60"
+				>Test description:</span
+			><br />{data.testContent.description}
+		</p>
+		<p class="flex items-center gap-2 mb-3">
+			<span
+				class="font-semibold text-light_text_black_60 dark:text-dark_text_white_60"
+				>Author:</span
+			>
+			<img
+				src={data.testContent.owner.image}
+				class="w-16 rounded-full"
+				alt="Author"
+			/>
 		</p>
 	</div>
-{:else}
-	{#if result}
-		<div class="sticky top-0 left-0 flex gap-2" in:fade>
-			<a href="/community">
-				<button class="btn">Back to community</button>
-			</a>
-			{#if data.session?.user}
-				<a href="/dashboard">
-					<button class="btn">Back to dashboard</button>
-				</a>
-			{/if}
-		</div>
-	{/if}
-	<Dialog bind:open={openDialog}>
-		<p class="text-center text-light_text_black dark:text-dark_text_white">
-			You are about to submit the test, after that you want be able to change
-			your answers, do you want to proceed?
-		</p>
-		<Space />
-		<div class="flex justify-center gap-3">
-			<button class="btn">Cancel</button>
-			<button
-				class="text-white btn btn-primary dark:bg-dark_primary dark:border-dark_primary hover:bg-light_secondary dark:hover:bg-dark_secondary"
-				on:click={async () => {
-					if (isSubmitting) return;
-					// Reset errors
-					for (let i in $testObject.questions) {
-						$testObject.questions[i].errors.content = '';
-					}
-
-					// Check test on the client first for all inputs filled and so on...
-					let clientCheck = checkTestClient($testObject);
-					if (!clientCheck['success']) {
-						$testObject = clientCheck['store'];
-						return;
-					}
-
-					isSubmitting = true;
-
-					// Then check the test on the server for the correct answers
-					// console.log('TEST', $testObject);
-					let res = await checkTestServerAndRecordIt($testObject);
-					if (!res['success']) {
-						submitError = res['error'] || 'Something went wrong';
-					} else {
-						submitError = '';
-						console.log(res);
-						if (!res['questionData']) return;
-						result = res['questionData'];
-						// ...
-					}
-
-					isSubmitting = false;
-				}}>Submit</button
-			>
-		</div>
-	</Dialog>
-	{#if $testObject}
-		<div class="mx-auto max-w-[650px]">
-			<h2 class="font-thin text-h3">{data.testContent.title}</h2>
-			<p class="text-light_text_black dark:text-dark_text_white_60">
-				{data.testContent.description}
-			</p>
-			<Space gap={40} />
-			{#each data.testContent.testVersions[0].questions as _, index}
-				<Input
-					questionIndex={index}
-					class={`border-2 border-solid ${
-						$testObject.questions[index].errors.content
-							? ' border-error'
-							: 'border-transparent'
-					}`}
-					resultFormat={result === null ? null : result[index]}
-				/>
-				<Space gap={10} />
-				{#if $testObject.questions[index].errors.content}
-					<p class="text-error dark:text-dark_error text-body2">
-						{$testObject.questions[index].errors.content}
+	<p class="flex items-center gap-1">
+		{#if markSystem}
+			<Collapsible position="left" title="Show marking">
+				<div class="grid__layout">
+					<p
+						class="font-semibold text-center text-light_text_black_60 dark:text-dark_text_white_60"
+					>
+						Grade
 					</p>
-				{/if}
-			{/each}
-			<Space gap={40} />
-			<div class="flex items-center justify-between">
-				{#if submitError}
-					<p class="text-error dark:text-dark_error text-body2">
-						{submitError}
-					</p>
-				{/if}
-				{#if result === null}
-					<BasicButton
-						title={'Check'}
-						onClick={async () => {
-							if (openDialog) {
-								openDialog();
-							}
-						}}
-						isLoading={isSubmitting}
-						class="ml-auto"
+					<Separator
+						w="100%"
+						h="100%"
+						class="mb-6"
+						color={$applicationStates.darkMode
+							? 'var(--dark-text-white-20)'
+							: 'var(--light-text-black-20)'}
 					/>
-				{/if}
-			</div>
-		</div>
-	{/if}
-{/if}
+					<p
+						class="font-semibold text-center text-light_text_black_60 dark:text-dark_text_white_60"
+					>
+						Range
+					</p>
+					<div class="col-span-3 mb-3" />
+					{#each markSystem as mark, index}
+						{#if mark['name'] !== undefined && mark['limit'] !== undefined}
+							{#if index === 0}
+								<p class="text-center">
+									{mark['name']}
+								</p>
+								<div />
+								<p class="text-center">
+									{MARK_LIMIT_MAX}% - {mark['limit']}%
+								</p>
+							{/if}
+							{#if markSystem[index - 1]?.limit !== undefined}
+								<p class="text-center">
+									{mark['name']}
+								</p>
+								<div />
+								<p class="text-center">
+									{markSystem[index - 1]['limit'] || 1 - 1}% - {mark['limit']}%
+								</p>
+							{/if}
+						{/if}
+					{/each}
+				</div>
+			</Collapsible>
+		{:else}
+			<iconify-icon icon="ic:round-close" class="text-3xl text-red-600" />
+		{/if}
+	</p>
+	<Space gap={30} />
+	<BasicButton title="Take test" />
+</div>
+
+<style>
+	.grid__layout {
+		display: grid;
+		grid-template-columns: 1fr 2px 1fr;
+	}
+</style>
---
+++ src/routes/(nonauthenticated)/tests/[id]/take/+page.svelte
@@ -0,0 +1,151 @@
+<script lang="ts">
+	import Input from '~components/testTaking/Input.svelte';
+	import { testObject } from '~stores/testObject';
+	import {
+		checkTestClient,
+		checkTestServerAndRecordIt,
+		initializeTestToTestStore
+	} from '~helpers/test';
+	import Space from '~components/separators/Space.svelte';
+	import BasicButton from '~components/buttons/BasicButton.svelte';
+	import Dialog from '~components/portals/Dialog.svelte';
+	import { fade } from 'svelte/transition';
+	import type { TestVersion } from '@prisma/client';
+	import Collapsible from '~components/collapsibles/Collapsible.svelte';
+	import MarkSystem from '~components/testCreator/creatorUtils/MarkSystem.svelte';
+	import { MARK_LIMIT_MAX, MARK_MAX } from '~schemas/textInput.js';
+	import Separator from '~components/separators/Separator.svelte';
+	import { applicationStates } from '~stores/applicationStates';
+
+	export let data;
+
+	let isTakingTest = false;
+
+	let isSubmitting = false;
+	let submitError: string = '';
+
+	let result: QuestionServerCheckResponse<QuestionContent>[] | null = null;
+
+	let openDialog: () => void;
+
+	// console.log(data.testContent);
+
+	initializeTestToTestStore(data.testContent);
+
+	// TODO: Create a check for the JSON so we make sure that the JSON is in correct format
+	let markSystem: MarkSystemJSON['marks'] | null;
+	if (data.testContent.testVersions[0].markSystemJSON) {
+		markSystem = JSON.parse(
+			data.testContent.testVersions[0].markSystemJSON?.toString()
+		) as MarkSystemJSON['marks'];
+	} else {
+		markSystem = null;
+	}
+
+	function startTest() {
+		isTakingTest = true;
+	}
+</script>
+
+{#if result}
+	<div class="sticky top-0 left-0 flex gap-2" in:fade>
+		<a href="/community">
+			<button class="btn">Back to community</button>
+		</a>
+		{#if data.session?.user}
+			<a href="/dashboard">
+				<button class="btn">Back to dashboard</button>
+			</a>
+		{/if}
+	</div>
+{/if}
+<Dialog bind:open={openDialog}>
+	<p class="text-center text-light_text_black dark:text-dark_text_white">
+		You are about to submit the test, after that you want be able to change your
+		answers, do you want to proceed?
+	</p>
+	<Space />
+	<div class="flex justify-center gap-3">
+		<button class="btn">Cancel</button>
+		<button
+			class="text-white btn btn-primary dark:bg-dark_primary dark:border-dark_primary hover:bg-light_secondary dark:hover:bg-dark_secondary"
+			on:click={async () => {
+				if (isSubmitting) return;
+				// Reset errors
+				for (let i in $testObject.questions) {
+					$testObject.questions[i].errors.content = '';
+				}
+
+				// Check test on the client first for all inputs filled and so on...
+				let clientCheck = checkTestClient($testObject);
+				if (!clientCheck['success']) {
+					$testObject = clientCheck['store'];
+					return;
+				}
+
+				isSubmitting = true;
+
+				// Then check the test on the server for the correct answers
+				// console.log('TEST', $testObject);
+				let res = await checkTestServerAndRecordIt($testObject);
+				if (!res['success']) {
+					submitError = res['error'] || 'Something went wrong';
+				} else {
+					submitError = '';
+					console.log(res);
+					if (!res['questionData']) return;
+					result = res['questionData'];
+					// ...
+				}
+
+				isSubmitting = false;
+			}}>Submit</button
+		>
+	</div>
+</Dialog>
+{#if $testObject}
+	<div class="mx-auto max-w-[650px]">
+		<h2 class="font-thin text-h3">{data.testContent.title}</h2>
+		<p class="text-light_text_black dark:text-dark_text_white_60">
+			{data.testContent.description}
+		</p>
+		<Space gap={40} />
+		{#each data.testContent.testVersions[0].questions as _, index}
+			<Input
+				questionIndex={index}
+				class={`border-2 border-solid ${
+					$testObject.questions[index].errors.content
+						? ' border-error'
+						: 'border-transparent'
+				}`}
+				resultFormat={result === null ? null : result[index]}
+			/>
+			<Space gap={10} />
+			{#if $testObject.questions[index].errors.content}
+				<p class="text-error dark:text-dark_error text-body2">
+					{$testObject.questions[index].errors.content}
+				</p>
+			{/if}
+		{/each}
+		<Space gap={40} />
+		<div class="flex items-center justify-between">
+			{#if submitError}
+				<p class="text-error dark:text-dark_error text-body2">
+					{submitError}
+				</p>
+			{/if}
+			{#if result === null}
+				<BasicButton
+					title={'Check'}
+					onClick={async () => {
+						if (openDialog) {
+							openDialog();
+						}
+					}}
+					isLoading={isSubmitting}
+					class="ml-auto"
+				/>
+			{/if}
+		</div>
+	</div>
+{/if}
