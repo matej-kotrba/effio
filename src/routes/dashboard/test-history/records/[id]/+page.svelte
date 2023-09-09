0819e3f508ac0dd30c90b599ca022ce3639496e9
mkit2009 <matejkotrba.skola@gmail.com>
2023-09-09 23:11:39 +0200

    Test record overview edit

--- src/routes/dashboard/test-history/records/[id]/+page.svelte
+++ src/routes/dashboard/test-history/records/[id]/+page.svelte
@@ -24,10 +24,12 @@
 
 <script lang="ts">
 	import Back from '~components/navigation/Back.svelte';
+	import Space from '~components/separators/Space.svelte';
 	import {
 		getMarkBasedOnPoints,
 		questionContentFunctions
 	} from '~helpers/test';
+	import { page } from '$app/stores';
 
 	export let data;
 </script>
@@ -38,6 +40,18 @@
 		<span class="loading loading-spinner loading-lg" />
 	</div>
 {:then res}
+	{#if res.record === null}
+		<div class="grid place-items-center">
+			<h4 class="text-h4">
+				<span class="text-h2">404</span> - This test record does not seem to exist
+				😕.
+			</h4>
+			<a
+				class="btn btn-outline dark:text-dark_text_white dark:outline-dark_text_white"
+				href="/dashboard">Back to dashboard</a
+			>
+		</div>
+	{/if}
 	{@const questionData = res.record?.questionRecords.map((question) => {
 		return checkJSONQuestionData(
 			// @ts-ignore
@@ -65,30 +79,48 @@
 			{@const mark = marks
 				? getMarkBasedOnPoints(marks, userPoints, maxPoints)
 				: undefined}
-			<div class="overflow-x-auto">
+			{@const takenAt = new Date(res.record.createdAt)}
+			<div
+				class="overflow-x-auto max-w-[700px] mx-auto dark:bg-dark_grey p-6 rounded-md"
+			>
+				<h3 class="font-semibold text-h5">{res.record.title}</h3>
+				<span class="text-light_text_black_80 dark:text-dark_text_white_80"
+					>Taken on {takenAt.getDate()}. {takenAt.getMonth() + 1}. {takenAt.getFullYear()}
+					- {takenAt.getHours()}:{takenAt.getMinutes()}</span
+				>
+				<Space gap={10} />
 				<table class="table">
 					<!-- head -->
 					<thead>
-						<tr>
-							<th>Mark</th>
-							<th>Your Points</th>
-							<th>Max Points</th>
-							<th>Percentage</th>
+						<tr class="text-light_text_black dark:text-dark_text_white">
+							<th class="font-medium text-center">Mark</th>
+							<th class="font-medium text-center">Your Points</th>
+							<th class="font-medium text-center">Max Points</th>
+							<th class="font-medium text-center">Percentage</th>
 						</tr>
 					</thead>
 					<tbody>
 						<tr>
 							{#if mark !== undefined}
-								<td>{mark.name}</td>
+								<td class="font-semibold text-center">{mark.name}</td>
 							{:else}
-								<td>Unknown</td>
+								<td class="font-semibold text-center">Unknown</td>
 							{/if}
-							<td>{userPoints}</td>
-							<td>{maxPoints}</td>
-							<td>{(userPoints / maxPoints || 0) * 100}%</td>
+							<td class="font-semibold text-center">{userPoints}</td>
+							<td class="font-semibold text-center">{maxPoints}</td>
+							<td class="font-semibold text-center"
+								>{(userPoints / maxPoints || 0) * 100}%</td
+							>
 						</tr>
 					</tbody>
 				</table>
+				<Space gap={10} />
+				<a
+					href={$page.url + '/detail'}
+					class="btn btn-outline dark:text-dark_text_white dark:outline-dark_text_white"
+				>
+					Details
+				</a>
 			</div>
 		{/if}
 	{/if}
