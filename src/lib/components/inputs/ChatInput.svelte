60244f319a1b3ac17f0e3c0d4b8a1577ea2ccf25
mkit2009 <matejkotrba.skola@gmail.com>
2023-10-01 23:36:08 +0200

    Working on chat input

--- src/lib/components/inputs/ChatInput.svelte
+++ src/lib/components/inputs/ChatInput.svelte
@@ -31,12 +31,42 @@
 	});
 </script>
 
-<div class={twMerge('fixed w-full max-w-full shadow z-10', classes)}>
+<div
+	class={twMerge(
+		'group fixed w-full max-w-full shadow z-10 flex items-start bg-white focus-within:outline-light_secondary duration-100 outline-2 outline outline-transparent rounded-l-md rounded-br-md rounded-tr-none',
+		classes
+	)}
+>
+	<div
+		class="absolute w-[60px] h-[30px] bg-white right-0 bottom-[calc(100%+2px)] outline-2 group-focus-within:outline-light_secondary duration-100 outline-transparent rounded-t-md input__out_box"
+	/>
 	<textarea
 		bind:this={textAreaRef}
 		rows="1"
-		class="w-full px-2 py-2 duration-150 rounded-md resize-none max-h-[380px] focus-within:outline-light_primary outline-2 outline outline-transparent"
+		class="w-full px-2 py-2 duration-150 rounded-l-md resize-none max-h-[380px] outline-none"
 		on:input={onInput}
 		on:keydown={onKeyDown}
 	/>
+	<button
+		type="button"
+		on:click={submitContent}
+		class="grid h-[43px] bg-white place-content-center rounded-r-md px-2 group"
+	>
+		<iconify-icon
+			icon="mingcute:send-plane-fill"
+			class="text-3xl duration-100 text-light_text_black group-hover:text-light_primary"
+		/>
+	</button>
 </div>
+
+<style>
+	.input__out_box::before {
+		content: '';
+		position: absolute;
+		width: 100%;
+		height: 2px;
+		top: 100%;
+		left: 0;
+		background-color: white;
+	}
+</style>
--- src/lib/schemas/textInput.ts
+++ src/lib/schemas/textInput.ts
@@ -1,6 +1,6 @@
 import { z } from "zod"
-import { isInArray } from "~helpers/zodCustom"
 
+// GENERAL ANSWER
 export const ANSWER_MIN = 1
 export const ANSWER_MAX = 100
 export const ANSWER_TITLE_MIN = 8
@@ -10,9 +10,11 @@ export const DESCRIPTION_MAX = 255
 export const TITLE_MIN = 8
 export const TITLE_MAX = 40
 
+// WRITE TYPE ANSWER SPECIFIC
 export const WRITE_ANSWER_MIN = 1
 export const WRITE_AMSWER_MAX = 30
 
+// MARKS AND ITS LIMITS
 export const MARK_MIN = 1
 export const MARK_MAX = 15
 export const MARK_LIMIT_MIN = 0
@@ -23,11 +25,17 @@ export const LIMIT_OPTIONS = [
   5
 ] as const;
 
+// GROUP GENERAL
 export const GROUP_NAME_MAX = 25
 export const GROUP_NAME_MIN = 5
 export const GROUP_DESCRIPTION_MAX = 255
 export const GROUP_DESCRIPTION_MIN = 0
 
+// CHAT INPUT SPECIFIC
+export const CHAT_INPUT_MAX = 255
+export const CHAT_INPUT_MIN = 1
+
+// SCHEMAS
 export const answerSchema = z.string().min(ANSWER_MIN, "Answer has to be at least 1 character long.").max(ANSWER_MAX, "Answer can be max 100 characters long.")
 export const titleSchema = z.string().min(TITLE_MIN, `Title has to be at least ${TITLE_MIN} character long.`).max(TITLE_MAX, `Title can be max ${TITLE_MAX} characters long.`)
 export const descriptionSchema = z.string().min(DESCRIPTION_MIN, "Tell the users what your test will be about with at least 15 characters.").max(DESCRIPTION_MAX, "Description can be max 255 characters long.")
@@ -36,3 +44,4 @@ export const markSchema = z.string().min(MARK_MIN, "Mark has to be at least 1 ch
 export const markLimitSchema = z.number({ required_error: "Please select a value" }).min(MARK_LIMIT_MIN, "Limit cannot be less than 0.").max(MARK_LIMIT_MAX, "Limit cannot be more than 100.")
 export const groupNameSchema = z.string().min(GROUP_NAME_MIN, `Mark has to be at least ${GROUP_NAME_MIN} character long.`).max(GROUP_NAME_MAX, `Mark can be max ${GROUP_NAME_MAX} characters long.`)
 export const groupDescriptionSchema = z.string().max(GROUP_DESCRIPTION_MAX, `Mark can be max ${GROUP_DESCRIPTION_MAX} characters long.`)
+export const chatInputSchema = z.string().min(CHAT_INPUT_MIN, "Message has to be at least 1 character long.").max(CHAT_INPUT_MAX, "Message can be max 255 characters long.")
