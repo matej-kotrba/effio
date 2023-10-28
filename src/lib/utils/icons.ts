ebc7fc3aaf99a118982828ce76a140ff14858491
mkit2009 <matejkotrba.skola@gmail.com>
2023-10-28 23:05:52 +0200

    WIP

---
+++ src/lib/utils/icons.ts
@@ -0,0 +1,3 @@
+export function userOrAnonymousSrc(url: string | null) {
+  return url ? url : "/imgs/svgs/user-circle.svg"
+}
--- src/routes/dashboard/my-groups/[name]/admin-test-overview/[subcategory]/[test]/UserTable.svelte
+++ src/routes/dashboard/my-groups/[name]/admin-test-overview/[subcategory]/[test]/UserTable.svelte
@@ -6,6 +6,9 @@
 	import type { User } from '@prisma/client';
 	import type { CreateObserverReturn } from '~/lib/utils/observers';
 	import { twMerge } from 'tailwind-merge';
+	import { userOrAnonymousSrc } from '~/lib/utils/icons';
+	import Separator from '~components/separators/Separator.svelte';
+	import { applicationStates } from '~stores/applicationStates';
 
 	type UserDataObject = Pick<User, 'name' | 'id' | 'image'> & {
 		takenCount: number;
@@ -89,28 +92,46 @@
 	});
 </script>
 
-<div class={twMerge('grid grid-cols-12', classes)}>
-	<div class="grid grid-cols-12 col-span-12">
-		<span class="col-span-3 font-bold text-center text-body1">Name</span>
-		<span class="col-span-1 font-bold text-center text-body1">Icon</span>
-		<span class="col-span-1 font-bold text-center text-body1">Taken</span>
-		<span class="col-span-2 font-bold text-center text-body1">Taken Count</span>
+<div
+	class={twMerge(
+		'grid grid-cols-12 max-w-[800px] max-h-[400px] overflow-y-scroll',
+		classes
+	)}
+>
+	<div class="grid grid-cols-7 col-span-12">
+		<span class="col-span-3 font-semibold text-center text-body1">Name</span>
+		<span class="col-span-1 font-semibold text-center text-body1">Icon</span>
+		<span class="col-span-1 font-semibold text-center text-body1">Taken</span>
+		<span class="col-span-2 font-semibold text-center text-body1"
+			>Taken Count</span
+		>
 	</div>
 	{#each users as user, index}
 		<div
 			use:addIntersectionUse={{ shouldActive: index === users.length - 1 }}
-			class="grid w-full grid-cols-12 col-span-12 px-2 py-2 bg-light_whiter text-light_text_black"
+			class="grid items-center w-full grid-cols-7 col-span-12 px-2 py-2 bg-light_whiter hover:bg-light_grey text-light_text_black"
 		>
 			<span class="col-span-3 font-semibold text-center">{user.name}</span>
-			<img
-				class="col-span-1 font-semibold text-center"
-				src={user.image}
-				alt="User"
-			/>
+			<div class="flex justify-center col-span-1 text-center">
+				<img
+					class="w-8 font-semibold rounded-full aspect-square text-light_text_black dark:text-dark_text_white"
+					src={userOrAnonymousSrc(user.image)}
+					alt="User"
+				/>
+			</div>
 			<span class="col-span-1 font-semibold text-center">{user.takenCount}</span
 			>
 			<span class="col-span-2 font-semibold text-center">{user.takenCount}</span
 			>
 		</div>
+		{#if index !== users.length - 1}
+			<Separator
+				w="100%"
+				h="1px"
+				class={$applicationStates.darkMode.isDarkMode
+					? 'var(--dark-text-white-20) col-span-12'
+					: 'var(--light-text-black-20) col-span-12'}
+			/>
+		{/if}
 	{/each}
 </div>
