c9fae7f46009bef5a51641c4ee34087036bfee22
mkit2009 <matejkotrba.skola@gmail.com>
2023-10-29 21:26:21 +0100

    Loading states added

--- src/routes/dashboard/my-groups/[name]/admin-test-overview/[subcategory]/[test]/UserTable.svelte
+++ src/routes/dashboard/my-groups/[name]/admin-test-overview/[subcategory]/[test]/UserTable.svelte
@@ -29,7 +29,8 @@
 	export { classes as class };
 
 	let users: UserDataObject[] = [];
-	let isFetchingNewUsers = true;
+	let isFetchingNewUsers = false;
+	let isFetchingBlank = true;
 
 	let ordering: Ordering = {
 		by: 'count',
@@ -37,10 +38,27 @@
 	};
 
 	function createOrderSwap(by: Ordering['by']) {
+		let initialOrder: Ordering['order'];
+
+		switch (by) {
+			case 'name': {
+				initialOrder = 'asc';
+				break;
+			}
+			case 'count': {
+				initialOrder = 'desc';
+				break;
+			}
+		}
+
 		return () => {
 			swapOrdering(
 				by,
-				ordering.by === by ? (ordering.order === 'asc' ? 'desc' : 'asc') : 'asc'
+				ordering.by === by
+					? ordering.order === 'desc'
+						? 'asc'
+						: 'desc'
+					: initialOrder
 			);
 		};
 	}
@@ -51,17 +69,25 @@
 			order
 		};
 
-		users = [];
-		getUsers();
+		getUsers(true);
 	}
 
-	async function getUsers() {
+	async function getUsers(shouldReset: boolean = false) {
+		if (shouldReset === true) {
+			isFetchingBlank = true;
+		} else {
+			isFetchingNewUsers = true;
+		}
+		await new Promise((resolve) => setTimeout(resolve, 2000));
 		const result = await trpc($page).groups.getGroupUsers.query({
 			groupId: data.groupId,
 			testId: data.testId,
 			subcategorySlug: data.subcategorySlug,
 			ordering: ordering,
-			cursor: users[users.length - 1] ? users[users.length - 1].id : undefined,
+			cursor:
+				shouldReset === false && users[users.length - 1]
+					? users[users.length - 1].id
+					: undefined,
 			limit: 12,
 			select: {
 				id: true,
@@ -69,14 +95,13 @@
 				image: true
 			}
 		});
+		if (shouldReset === true) {
+			isFetchingBlank = false;
+		} else {
+			isFetchingNewUsers = false;
+		}
 
-		// const userTestCount = await trpc(
-		// 	$page
-		// ).groups.getUsersTestRecordCount.query({
-		// 	subcategorySlug: data.subcategorySlug,
-		// 	testId: data.testId,
-		// 	userId: result.map((item) => item.id)
-		// });
+		if (shouldReset) users = [];
 
 		const transformedArray: UserDataObject[] = result.map((item) => {
 			return {
@@ -104,7 +129,7 @@
 		});
 		addIntersectionUse = addIntersection;
 
-		getUsers();
+		getUsers(true);
 
 		return () => {
 			observer.disconnect();
@@ -114,56 +139,88 @@
 
 <div
 	class={twMerge(
-		'grid grid-cols-12 max-w-[800px] h-[400px] max-h-[800px] shadow-md',
+		'grid grid-cols-12 max-w-[800px] h-[400px] max-h-[800px] shadow-md bg-light_whiter',
 		classes
 	)}
 >
-	<div class="grid grid-cols-7 col-span-12 py-1 bg-light_whiter rounded-t-md">
-		<div class="relative">
+	<div
+		class="grid grid-cols-7 col-span-12 py-1 bg-light_whiter rounded-t-md h-fit"
+	>
+		<div class="relative flex justify-center col-span-3 h-fit">
 			<button
 				type="button"
-				on:click={() => createOrderSwap('name')}
-				class="col-span-3 font-semibold text-center text-body1">Name</button
+				on:click={() => createOrderSwap('name')()}
+				class="font-semibold text-center text-body1">Name</button
 			>
-		</div>
-		<span class="col-span-1 font-semibold text-center text-body1">Icon</span>
-		<span class="col-span-1 font-semibold text-center text-body1">Taken</span>
-		<button
-			type="button"
-			class="col-span-2 font-semibold text-center text-body1"
-			on:click={() => createOrderSwap('count')}>Taken Count</button
-		>
-	</div>
-	<div class="grid grid-cols-7 col-span-12 overflow-y-scroll">
-		{#each users as user, index}
-			<div
-				use:addIntersectionUse={{ shouldActive: index === users.length - 1 }}
-				class="grid items-center w-full grid-cols-7 col-span-12 px-2 py-2 bg-light_whiter hover:bg-light_grey text-light_text_black"
-			>
-				<span class="col-span-3 font-semibold text-center">{user.name}</span>
-				<div class="flex justify-center col-span-1 text-center">
-					<img
-						class="w-8 font-semibold rounded-full aspect-square text-light_text_black dark:text-dark_text_white"
-						src={userOrAnonymousSrc(user.image)}
-						alt="User"
-					/>
-				</div>
-				<span class="col-span-1 font-semibold text-center"
-					>{user.takenCount}</span
-				>
-				<span class="col-span-2 font-semibold text-center"
-					>{user.takenCount}</span
-				>
-			</div>
-			{#if index !== users.length - 1}
-				<Separator
-					w="100%"
-					h="1px"
-					class={$applicationStates.darkMode.isDarkMode
-						? 'var(--dark-text-white-20) col-span-12'
-						: 'var(--light-text-black-20) col-span-12'}
+			{#if ordering.by === 'name'}
+				<iconify-icon
+					icon="iconamoon:arrow-down-2"
+					class={`absolute right-0 text-2xl -translate-y-1/2 top-1/2 text-light_text_black dark:text-dark_text_white ${
+						ordering.order === 'asc' ? 'rotate-180' : ''
+					}`}
 				/>
 			{/if}
-		{/each}
+		</div>
+		<span class="col-span-1 font-semibold text-center text-body1 h-fit"
+			>Icon</span
+		>
+		<span class="col-span-1 font-semibold text-center text-body1 h-fit"
+			>Taken</span
+		>
+		<div class="relative flex justify-center col-span-2 h-fit">
+			<button
+				type="button"
+				class="col-span-2 font-semibold text-center text-body1"
+				on:click={() => createOrderSwap('count')()}>Taken Count</button
+			>
+			{#if ordering.by === 'count'}
+				<iconify-icon
+					icon="iconamoon:arrow-down-2"
+					class={`absolute right-0 text-2xl -translate-y-1/2 top-1/2 text-light_text_black dark:text-dark_text_white ${
+						ordering.order === 'asc' ? 'rotate-180' : ''
+					}`}
+				/>
+			{/if}
+		</div>
+	</div>
+	<div class="grid grid-cols-7 col-span-12 overflow-y-scroll bg-light_whiter">
+		{#if isFetchingBlank && users.length === 0}
+			<div class="flex justify-center col-span-7">
+				<span class="loading loading-bars loading-lg" />
+			</div>
+		{:else}
+			{#each users as user, index}
+				<div
+					use:addIntersectionUse={{ shouldActive: index === users.length - 1 }}
+					class={`grid items-center w-full grid-cols-7 col-span-12 px-2 py-2 bg-light_whiter hover:bg-light_grey text-light_text_black ${
+						isFetchingBlank ? 'opacity-40' : ''
+					}`}
+				>
+					<span class="col-span-3 font-normal text-center">{user.name}</span>
+					<div class="flex justify-center col-span-1 text-center">
+						<img
+							class="w-8 font-normal rounded-full aspect-square text-light_text_black dark:text-dark_text_white"
+							src={userOrAnonymousSrc(user.image)}
+							alt="User"
+						/>
+					</div>
+					<span class="col-span-1 font-normal text-center"
+						>{user.takenCount}</span
+					>
+					<span class="col-span-2 font-normal text-center"
+						>{user.takenCount}</span
+					>
+				</div>
+				{#if index !== users.length - 1}
+					<Separator
+						w="100%"
+						h="1px"
+						class={$applicationStates.darkMode.isDarkMode
+							? 'var(--dark-text-white-20) col-span-12'
+							: 'var(--light-text-black-20) col-span-12'}
+					/>
+				{/if}
+			{/each}
+		{/if}
 	</div>
 </div>
