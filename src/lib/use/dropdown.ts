c344201492eb23520ab6cc7737cea31f40156047
mkit2009 <matejkotrba.skola@gmail.com>
2023-11-01 22:54:05 +0100

    Updated dropdown look and behaviour

--- src/lib/use/dropdown.ts
+++ src/lib/use/dropdown.ts
@@ -1,16 +1,19 @@
 import type { Action } from "svelte/types/runtime/action/index"
 
 export const dropdown: Action<HTMLElement> = (node, text) => {
+  let lastAnim: Animation
+
   const divDropdown = window.document.createElement("div")
   divDropdown.classList.add("dropdown")
   divDropdown.classList.add("absolute")
   divDropdown.classList.add("pointer-events-none")
 
   divDropdown.innerHTML = `
-    <div class="btn bg-light_text_black dark:bg-dark_light_grey text-white dropdown-toggle duration-150 text-body1" type="button" data-toggle="dropdown" aria-expanded="false">
+    <div class="btn shadow-sm shadow-light_text_black_20 dark:shadow-dark_text_white_20 dark:bg-dark_light_grey dark:text-white text-light_text_black bg-light_white dropdown-toggle border-transparent duration-150 text-body2" type="button" data-toggle="dropdown" aria-expanded="false">
       ${text}
     </div>
   `
+  divDropdown.style.opacity = "0"
 
 
   window.document.querySelector("body")?.appendChild(divDropdown)
@@ -23,11 +26,11 @@ export const dropdown: Action<HTMLElement> = (node, text) => {
   }
 
   function appear() {
-    divDropdown.animate([{ opacity: 0 }, { opacity: 1 }], { duration: 150, fill: "forwards" })
+    lastAnim = divDropdown.animate([{ opacity: 0 }, { opacity: 1 }], { duration: 150, fill: "forwards", delay: 500 })
   }
 
   function disappear() {
-    divDropdown.animate([{ opacity: 1 }, { opacity: 0 }], { duration: 150, fill: "forwards" })
+    divDropdown.animate([{ opacity: lastAnim.playState === "finished" ? 1 : divDropdown.style.opacity }, { opacity: 0 }], { duration: 150, fill: "forwards" })
   }
 
   node.addEventListener("mouseenter", appear)
