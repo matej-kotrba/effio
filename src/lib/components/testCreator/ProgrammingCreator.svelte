3df026b0fc95459d2a3996a246698a560e56407b
mkit2009 <matejkotrba.skola@gmail.com>
2023-11-26 22:19:55 +0100

    Add programming description input field to ProgrammingCreator.svelte

--- src/lib/components/testCreator/ProgrammingCreator.svelte
+++ src/lib/components/testCreator/ProgrammingCreator.svelte
@@ -2,11 +2,19 @@
 	import { onMount } from 'svelte';
 
 	import TextInput from '~components/inputs/TextInput.svelte';
+	import TextInputSimple from '~components/inputs/TextInputSimple.svelte';
 	import { questionContentFunctions } from '~helpers/test';
-	import { answerSchema } from '~schemas/textInput';
+	import {
+		answerSchema,
+		programminDescriptionSchema
+	} from '~schemas/textInput';
 	import { testObject } from '~stores/testObject';
 
-	let content = $testObject.questions[0].content as ProgrammingQuestion;
+	// Programming question can only be one on in the creator, that's why we use 0 as index
+	const INDEX_OF_QUESTION = 0;
+
+	let content = $testObject.questions[INDEX_OF_QUESTION]
+		.content as ProgrammingQuestion;
 
 	onMount(() => {
 		$testObject.questions = [];
@@ -32,7 +40,17 @@
 		title="Code name"
 		titleName="codeName"
 		validationSchema={answerSchema}
-		on:error={(event) => (content.answers[index].error = event.detail)}
-		bind:inputValue={content.answers[index].answer}
+		on:error={(event) =>
+			($testObject.questions[INDEX_OF_QUESTION]['errors']['title'] =
+				event.detail)}
+		bind:inputValue={$testObject.questions[INDEX_OF_QUESTION]['title']}
+	/>
+	<TextInputSimple
+		title="Describe what the code should do"
+		titleName="description"
+		validationSchema={programminDescriptionSchema}
+		on:error={(event) => (content['errors']['description'] = event.detail)}
+		bind:inputValue={content['description']}
+		displayOutside={true}
 	/>
 </div>
--- src/lib/schemas/textInput.ts
+++ src/lib/schemas/textInput.ts
@@ -48,12 +48,16 @@ export const GEOGRAPHY_TOLERANCE_DEFAULT = 400
 export const GEOGRAPHY_ZOOM_MIN = 2
 export const GEOGRAPHY_ZOOM_MAX = 18
 
+// PROGRAMMING SPECIFIC
+export const PROGRAMMING_DESCRIPTION_MIN = 15
+export const PROGRAMMING_DESCRIPTION_MAX = 1024
+
 // SCHEMAS
-export const answerSchema = z.string().min(ANSWER_MIN, "Answer has to be at least 1 character long.").max(ANSWER_MAX, "Answer can be max 100 characters long.")
+export const answerSchema = z.string().min(ANSWER_MIN, `Answer has to be at least ${ANSWER_MIN} character long.`).max(ANSWER_MAX, `Answer can be max ${ANSWER_MAX} characters long.`)
 export const titleSchema = z.string().min(TITLE_MIN, `Title has to be at least ${TITLE_MIN} characters long.`).max(TITLE_MAX, `Title can be max ${TITLE_MAX} characters long.`)
-export const descriptionSchema = z.string().min(DESCRIPTION_MIN, "Tell the users what your test will be about with at least 15 characters.").max(DESCRIPTION_MAX, "Description can be max 255 characters long.")
+export const descriptionSchema = z.string().min(DESCRIPTION_MIN, `Tell the users what your test will be about with at least ${DESCRIPTION_MIN} characters.`).max(DESCRIPTION_MAX, `Description can be max ${DESCRIPTION_MAX} characters long.`)
 export const writeAnswerSchema = z.string().min(WRITE_ANSWER_MIN, "Write answer has to be at least 1 character long.").max(WRITE_AMSWER_MAX, "Answer can be max 30 characters long.")
-export const markSchema = z.string().min(MARK_MIN, "Mark has to be at least 1 character long.").max(MARK_MAX, "Mark can be max 15 characters long.")
+export const markSchema = z.string().min(MARK_MIN, `Mark has to be at least ${MARK_MIN} character long.`).max(MARK_MAX, `Mark can be max ${MARK_MAX} characters long.`)
 export const markLimitSchema = z.number({ required_error: "Please select a value" }).min(MARK_LIMIT_MIN, "Limit cannot be less than 0.").max(MARK_LIMIT_MAX, "Limit cannot be more than 100.")
 export const groupNameSchema = z.string().min(GROUP_NAME_MIN, `Mark has to be at least ${GROUP_NAME_MIN} character long.`).max(GROUP_NAME_MAX, `Mark can be max ${GROUP_NAME_MAX} characters long.`)
 export const groupDescriptionSchema = z.string().max(GROUP_DESCRIPTION_MAX, `Mark can be max ${GROUP_DESCRIPTION_MAX} characters long.`)
@@ -62,3 +66,4 @@ export const geographyToleranceSchema = z.number({ invalid_type_error: "Input ha
 export const latitudeSchema = z.number({ invalid_type_error: "Input has to be a valid number" }).min(LATITUDE_MIN, "Number has to be bigger than -90").max(LATITUDE_MAX, "Number has to be smaller than 90");
 export const longitudeSchema = z.number({ invalid_type_error: "Input has to be a valid number" }).min(LONGITUDE_MIN, "Number has to be bigger than -180").max(LONGITUDE_MAX, "Number has to be smaller than 180");
 export const geographyLocationSchema = z.tuple([latitudeSchema, longitudeSchema], { invalid_type_error: "Latitude and longitude has to be valid numbers." })
+export const programminDescriptionSchema = z.string().min(ANSWER_MIN, `Description has to be at least ${PROGRAMMING_DESCRIPTION_MIN} character long.`).max(ANSWER_MAX, `Description can be max ${PROGRAMMING_DESCRIPTION_MAX} characters long.`)
