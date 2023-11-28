9966472a833c1346f6ce130b77b5af08755d964c
mkit2009 <matejkotrba.skola@gmail.com>
2023-11-28 22:36:42 +0100

    Add Space component and implement test dropdown functionality

--- src/lib/components/testCreator/programmingCreator/InputOutputSetter.svelte
+++ src/lib/components/testCreator/programmingCreator/InputOutputSetter.svelte
@@ -5,6 +5,7 @@
 	import type { editor } from 'monaco-editor';
 	import { browser } from '$app/environment';
 	import { testObject } from '~stores/testObject';
+	import Space from '~components/separators/Space.svelte';
 
 	export let questionIndex: number;
 	export let title = '';
@@ -12,10 +13,19 @@
 	$: content = $testObject.questions[questionIndex]
 		.content as ProgrammingQuestion;
 
-	let editorContainer: HTMLDivElement;
+	let inputEditorContainer: HTMLDivElement;
+	let outputEditorContainer: HTMLDivElement;
 
 	let monaco: typeof import('monaco-editor');
-	let editor: editor.IStandaloneCodeEditor;
+	let inputEditor: editor.IStandaloneCodeEditor;
+	let outputEditor: editor.IStandaloneCodeEditor;
+
+	let openDialog: () => void;
+	let currentDropdownData = {
+		index: 0,
+		input: '',
+		output: ''
+	};
 
 	// export let selectedType: (typeof SUPPORTED_TYPES)[number] | undefined =
 	// 	undefined;
@@ -27,6 +37,15 @@
 	// ) {
 	// 	selectedType = e.currentTarget.value as (typeof SUPPORTED_TYPES)[number];
 	// }
+
+	function openDropdown(index: number) {
+		currentDropdownData = {
+			index: index,
+			input: content.tests[index].input,
+			output: content.tests[index].output
+		};
+		openDialog();
+	}
 
 	onMount(async () => {
 		monaco = await import('monaco-editor');
@@ -41,31 +60,59 @@
 				}
 			};
 		});
-		editor = monaco.editor.create(editorContainer, {
-			value: '{\n\t\n}',
+		inputEditor = monaco.editor.create(inputEditorContainer, {
+			value: '',
 			language: 'json',
-			theme: 'vs-dark-plus'
+			theme: 'vs-dark'
+		});
+		outputEditor = monaco.editor.create(outputEditorContainer, {
+			value: '',
+			language: 'json',
+			theme: 'vs-dark'
 		});
 
-		window.addEventListener('resize', () => editor.layout());
+		window.addEventListener('resize', () => inputEditor.layout());
+		window.addEventListener('resize', () => outputEditor.layout());
 	});
 
 	onDestroy(() => {
 		if (browser) {
-			window.removeEventListener('resize', () => editor.layout());
-			editor.dispose();
+			window.removeEventListener('resize', () => inputEditor.layout());
+			window.removeEventListener('resize', () => outputEditor.layout());
+			inputEditor.dispose();
+			outputEditor.dispose();
 		}
 	});
 </script>
 
-<Dialog title="Test edit">
-	<div id="container" bind:this={editorContainer} />
+<Dialog title="Test edit" bind:open={openDialog}>
+	<span class="text-h6">Input </span><span
+		class="text-body2 text-light_text_black_60 dark:text-dark_text_white_60"
+		>for client code</span
+	>
+	<div
+		class="w-full h-[100px] rounded-md overflow-hidden"
+		bind:this={inputEditorContainer}
+	/>
+	<Space gap={20} />
+	<span class="text-h6">Output </span><span
+		class="text-body2 text-light_text_black_60 dark:text-dark_text_white_60"
+		>for client code</span
+	>
+	<div
+		class="w-full h-[100px] rounded-md overflow-hidden"
+		bind:this={outputEditorContainer}
+	/>
 </Dialog>
 <div>
 	<h6>{title}</h6>
 	<div>
 		{#each content['tests'] as test, index}
-			<div>Test {index + 1}</div>
+			<button
+				type="button"
+				class="btn min-w-[240px]"
+				on:click={() => openDropdown(index)}>Test {index + 1}</button
+			>
 		{/each}
 	</div>
 </div>
