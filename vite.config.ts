9be0b80c8cd115c38f6a074682b7dc09e40a2703
matej-kotrba <matejkotrba.skola@gmail.com>
2023-12-28 21:32:46 +0100

    Fixed Vite config to work with web workers in production

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	worker: {
+		plugins: [sveltekit()],
+	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	},
