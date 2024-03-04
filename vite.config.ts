907977e500628fdd7eaa82e1212a0b3259cca5bf
matej-kotrba <matejkotrba.skola@gmail.com>
2024-03-04 19:11:43 +0100

    fix: Edited Vite config for web worker support

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,6 @@ import path from "path"
 
 export default defineConfig({
 	plugins: [sveltekit()],
-	worker: {
-		plugins: [sveltekit()],
-	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	},
