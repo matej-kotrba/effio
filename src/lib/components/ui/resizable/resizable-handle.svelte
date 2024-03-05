8f190117041560314fecd8e9c8659ee9395f5123
Matěj Kotrba <77980040+matej-kotrba@users.noreply.github.com>
2024-03-05 22:28:52 +0100

    Recreating programming test UI (#9)

--- src/lib/components/ui/resizable/resizable-handle.svelte
+++ src/lib/components/ui/resizable/resizable-handle.svelte
@@ -2,6 +2,7 @@
 	import DragHandleDots2 from 'svelte-radix/DragHandleDots2.svelte';
 	import * as ResizablePrimitive from 'paneforge';
 	import { cn } from '~utils/utils';
+	import { createEventDispatcher } from 'svelte';
 
 	type $$Props = ResizablePrimitive.PaneResizerProps & {
 		withHandle?: boolean;
@@ -11,9 +12,16 @@
 	export let el: $$Props['el'] = undefined;
 	let className: $$Props['class'] = undefined;
 	export { className as class };
+
+	const dispatch = createEventDispatcher();
+
+	function onResize(isDragging: boolean) {
+		dispatch('resizedContainer', { isDragging: isDragging });
+	}
 </script>
 
 <ResizablePrimitive.PaneResizer
+	onDraggingChange={onResize}
 	bind:el
 	class={cn(
 		'relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 data-[direction=vertical]:h-px data-[direction=vertical]:w-full data-[direction=vertical]:after:left-0 data-[direction=vertical]:after:h-1 data-[direction=vertical]:after:w-full data-[direction=vertical]:after:-translate-y-1/2 data-[direction=vertical]:after:translate-x-0 [&[data-direction=vertical]>div]:rotate-90',
--- src/routes/(nonauthenticated)/tests/[id]/take/Hints.svelte
+++ src/routes/(nonauthenticated)/tests/[id]/take/Hints.svelte
@@ -4,7 +4,57 @@
 	export let hintIndex: number = 0;
 </script>
 
-{#if hints.length !== 0}
+<div class="flex items-end justify-between">
+	<h4>Hints</h4>
+	<div class="text-body2">
+		<span>{hintIndex + 1}</span>
+		<span class="text-light_text_black_40">/</span>
+		<span>{hints.length}</span>
+	</div>
+</div>
+{#if hints.length === 0}
+	No hint provided
+{:else}
+	<div
+		class="grid items-center py-1 border rounded-lg border-light_text_black_40 bg-light_whiter grid__container"
+	>
+		<button
+			type="button"
+			class="grid duration-100 group disabled:hover:px-0 hover:pl-4 place-content-center"
+			disabled={hintIndex === 0}
+			on:click={() => {
+				if (hintIndex > 0) {
+					hintIndex--;
+				}
+			}}
+		>
+			<iconify-icon
+				icon="iconamoon:arrow-left-2-light"
+				class="text-2xl text-light_text_black group-disabled:text-light_text_black_40"
+			/>
+		</button>
+		<span>
+			{hints[hintIndex]}
+		</span>
+		<button
+			type="button"
+			class="grid duration-100 group disabled:hover:px-0 hover:pr-4 place-content-center"
+			disabled={hintIndex === hints.length - 1}
+			on:click={() => {
+				if (hintIndex < hints.length - 1) {
+					hintIndex++;
+				}
+			}}
+		>
+			<iconify-icon
+				icon="iconamoon:arrow-right-2-light"
+				class="text-2xl text-light_text_black group-disabled:text-light_text_black_40"
+			/>
+		</button>
+	</div>
+{/if}
+
+<!-- {#if hints.length !== 0}
 	<div>
 		{#if !isHintShowed}
 			<button
@@ -54,4 +104,10 @@
 			{/key}
 		{/if}
 	</div>
-{/if}
+{/if} -->
+
+<style>
+	.grid__container {
+		grid-template-columns: auto 1fr auto;
+	}
+</style>
