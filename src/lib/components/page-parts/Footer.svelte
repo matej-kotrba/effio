a3e0ff9e287a394613cfb6948fc2eda14a591749
matej-kotrba <matejkotrba.skola@gmail.com>
2024-03-19 15:41:32 +0100

    WIP

--- src/lib/components/page-parts/Footer.svelte
+++ src/lib/components/page-parts/Footer.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
-	import BackgroundBeams from '~components/ui/beams/BackgroundBeams.svelte';
 	import Separator from '../separators/Separator.svelte';
 	import Space from '~components/separators/Space.svelte';
+	import { fly } from 'svelte/transition';
+	import { intersect } from '~use/intersectionObserver';
+
+	let displayName = false;
 </script>
 
-<footer class="relative bg-light_terciary">
+<footer class="relative bg-light_terciary dark:bg-dark_terciary">
 	<div
 		class="container relative flex flex-col items-start justify-between gap-10 px-10 py-20 mx-auto md:flex-row"
 	>
@@ -12,20 +15,17 @@
 			class="flex flex-col items-center gap-4 mx-auto md:flex-row md:items-start md:mx-0"
 		>
 			<div class="flex flex-col justify-center w-max">
-				<h5
-					class="font-semibold text-center text-light_whiter dark:text-light_text_black text-h5"
-				>
+				<h5 class="font-semibold text-center text-light_whiter text-h5">
 					Do you find Effio usefull?
 				</h5>
-				<span
-					class="text-center text-dark_text_white_80 dark:text-light_text_black_80 text-body2"
+				<span class="text-center text-dark_text_white_80 text-body2"
 					>Leave a star on GitHub</span
 				>
 				<a
 					href="https://github.com/matej-kotrba/effio"
 					target="_blank"
 					referrerpolicy="no-referrer"
-					class="mx-auto text-light_whiter dark:text-light_text_black"
+					class="mx-auto text-light_whiter"
 				>
 					<iconify-icon icon="mdi:github" class="mx-auto text-[3rem]" />
 				</a>
@@ -39,19 +39,19 @@
 			<div class="flex flex-col items-start">
 				<h6 class="font-semibold text-h6 text-light_whiter">Page links</h6>
 				<a
-					class="duration-100 text-dark_text_white_80 dark:text-light_text_black_60 hover:text-dark_text_white dark:hover:text-dark_text_white text-body1"
+					class="duration-100 text-dark_text_white_80 hover:text-dark_text_white dark:hover:text-dark_text_white text-body1"
 					href="/">Home</a
 				>
 				<a
-					class="duration-100 text-dark_text_white_80 dark:text-light_text_black_60 hover:text-dark_text_white dark:hover:text-dark_text_white text-body1"
+					class="duration-100 text-dark_text_white_80 hover:text-dark_text_white dark:hover:text-dark_text_white text-body1"
 					href="/login">Log in</a
 				>
 				<a
-					class="duration-100 text-dark_text_white_80 dark:text-light_text_black_60 hover:text-dark_text_white dark:hover:text-dark_text_white text-body1"
+					class="duration-100 text-dark_text_white_80 hover:text-dark_text_white dark:hover:text-dark_text_white text-body1"
 					href="/dashboard">Dashboard</a
 				>
 				<a
-					class="duration-100 text-dark_text_white_80 dark:text-light_text_black_60 hover:text-dark_text_white dark:hover:text-dark_text_white text-body1"
+					class="duration-100 text-dark_text_white_80 hover:text-dark_text_white dark:hover:text-dark_text_white text-body1"
 					href="/community">Community</a
 				>
 			</div>
@@ -72,6 +72,31 @@
 					/>
 				</a>
 			</div>
+		</div>
+	</div>
+	<div class="w-full p-2 text-center">
+		<h6>Created with by</h6>
+		<div
+			class="flex justify-center gap-1 overflow-hidden font-bold text-center text-h4"
+			use:intersect
+			on:intersect={() => (displayName = true)}
+			on:unintersect={() => (displayName = false)}
+		>
+			{#if displayName}
+				{#each 'Matěj Kotrba' as letter, index (letter + index)}
+					<div
+						in:fly|global={{
+							duration: 250,
+							delay: index * 150,
+							y: 100
+						}}
+					>
+						<span>{letter}</span>
+					</div>
+				{/each}
+			{:else}
+				<span class="w-0 overflow-hidden opacity-0">Matěj Kotrba</span>
+			{/if}
 		</div>
 	</div>
 </footer>
