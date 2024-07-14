2f47225e21134e88ee80e652c6f5bbbd0b39c6d1
matej-kotrba <matejkotrba.skola@gmail.com>
2024-07-14 10:58:56 +0200

    be: added redis call to keep instance alive

--- src/routes/api/supabase/keep-alive/+server.ts
+++ src/routes/api/supabase/keep-alive/+server.ts
@@ -1,11 +1,13 @@
 import type { Tag } from '@prisma/client';
 import prisma from '~/lib/prisma.js';
+import { redis } from '~/lib/server/redis/redis';
 
 export const GET = async () => {
   let tags: Tag[] = []
   try {
     // Just some data fetching to keep the Supabase instance alive
     tags = await prisma.tag.findMany();
+    redis.set('keep-alive', Math.random().toString())
   }
   catch {
     return new Response('Error fetching data', { status: 500 });
